c9e78ab44e0f8ed7a54b2ac5286e65831c2c14d4
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-04 11:17:28 -0700

    refine the javascript attempt to remove previous class
    Co-authored-by: Joseph Pearce

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
   $("form#age").submit(function(event){
     event.preventDefault();
-    const dob = parseInt($("#born").val());
-    console.log(dob);
-
-  
+    // const dob = $("input#born").val();
+    const age = parseInt($("input#age").val());
+    
     if (age > 18) {
       $('.over18').show();
     } else if (age === 18) {
@@ -12,5 +11,8 @@ $(document).ready(function() {
     } else if (age < 18) {
       $('.under18').show();
     }
+    // $("body").removeClass(".over18, .ofAge, .under18");
   });
 });
+
+
