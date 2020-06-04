50a661fc797a90e2d2d04dca956e6d4cf8c8ce3f
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-04 11:25:55 -0700

    Hide previous input age and output
    Co-authored-by: Joseph Pearce

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $("form#age").submit(function(event){
     event.preventDefault();
-    // const dob = $("input#born").val();
     const age = parseInt($("input#age").val());
+    $('.under18').hide();
+    $('.ofAge').hide();
+    $('.over18').hide();
     
     if (age > 18) {
       $('.over18').show();
@@ -11,7 +13,6 @@ $(document).ready(function() {
     } else if (age < 18) {
       $('.under18').show();
     }
-    // $("body").removeClass(".over18, .ofAge, .under18");
   });
 });
 
